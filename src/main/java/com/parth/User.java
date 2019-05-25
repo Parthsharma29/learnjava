831d81da47126c7433408ac9a8d44b501e779ba6
Parth Sharma <parthsharma46@gmail.com>
2019-05-25 11:05:27 +0530

    employee

--- src/main/java/com/parth/User.java
+++ src/main/java/com/parth/User.java
@@ -1,7 +1,54 @@
 package com.parth;
 
 public class User {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
+    String firstName;
+    String lastName;
+    String email;
+    String country;
+
+    
+    User(String firstName, String lastName ){
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
+
+    User(String firstName, String lastName, String email) {
+        this(firstName, lastName);   
+        this.email = email;
+     }
+
+
+    public void sayName() {
+        System.out.println("Hello! My name is "+firstName+" "+lastName);
+    }
+
+    public void saluation() {
+        String gender = "male";
+        String title = "Mr";
+        if(gender == "male") {
+            title = "Mr";
+        }   
+        else {
+            title = "Miss";
+        }
+
+        System.out.println(title);
+    }
+  
+
+    public static void main(String[] args) {
+
+        User user2 = new User("James","Ray", "james@gmail.com");
+        System.out.println(user2);
+
+      
+
+    }
+
+    public String toString() {
+        return this.firstName+" "+this.lastName+" "+this.email+" "+this.country;
+        
+    }
+
+
 }
---
+++ src/main/java/com/parth/employee.java
@@ -0,0 +1,31 @@
+package com.parth;
+
+public class employee {
+
+  String firstname1 ;
+  String lastname;
+  String d_o_b;
+  String joiningdate;
+  String department;
+
+
+
+
+employee(String firstname1 ,String lastname, String d_o_b,String joiningdate,String department ){
+
+    this.firstname1 = firstname1 ;
+    this.lastname = lastname;
+    this.d_o_b = d_o_b;
+    this.joiningdate=joiningdate;
+    this.department = department;
+}
+
+public static void main(String[] args) {
+    employee emp=new employee("abc", "xyz", "ddd", "29/11/1654", "it");
+    System.out.println(emp);
+}
+ public String toString(){
+
+ return  this.firstname1+ " " +this.lastname+ " " +this.d_o_b +" "+this.department;
+}
+}
